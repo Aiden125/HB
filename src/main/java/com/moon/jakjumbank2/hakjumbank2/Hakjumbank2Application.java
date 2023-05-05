5246255717a0093a7ee1c74877620c45fad5b16a
HeeSeok <mungml1995@gmail.com>
2023-05-05 18:12:29 +0900

    first commit

---
+++ src/main/java/com/moon/jakjumbank2/hakjumbank2/Hakjumbank2Application.java
@@ -0,0 +1,13 @@
+package com.moon.jakjumbank2.hakjumbank2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hakjumbank2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hakjumbank2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/moon/jakjumbank2/hakjumbank2/Hakjumbank2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.jakjumbank2.hakjumbank2;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hakjumbank2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
