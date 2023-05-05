ad10e17054ef713eb676d62732e7268302991029
HeeSeok <mungml1995@gmail.com>
2023-05-05 20:25:54 +0900

    base

---
+++ src/test/java/com/moon/jakjumbank2/hakjumbank2/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,25 @@
+package com.moon.jakjumbank2.hakjumbank2.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_테스트() {
+        //given
+        String name = "문";
+        int amount = 1;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        Assertions.assertThat(dto.getName()).isEqualTo(name);
+        Assertions.assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+}
