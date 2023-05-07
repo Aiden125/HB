d136a1e4fb120645c224f10483dbd0f16c50fac9
HeeSeok <mungml1995@gmail.com>
2023-05-07 16:55:44 +0900

    baseTimeEntity add // 230507 3

--- src/main/java/com/moon/jakjumbank2/hakjumbank2/Hakjumbank2Application.java
+++ src/main/java/com/moon/jakjumbank2/hakjumbank2/Hakjumbank2Application.java
@@ -2,7 +2,9 @@ package com.moon.jakjumbank2.hakjumbank2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Hakjumbank2Application {
 
---
+++ src/main/java/com/moon/jakjumbank2/hakjumbank2/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.moon.jakjumbank2.hakjumbank2.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // 엔티티가 아님을 명시
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
