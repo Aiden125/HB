bc098ed0d3f00f3d1bf593b0a4d8b5447db73469
HeeSeok <mungml1995@gmail.com>
2023-05-21 22:54:40 +0900

    postsService

--- src/main/java/com/moon/jakjumbank2/hakjumbank2/service/posts/PostsService.java
+++ src/main/java/com/moon/jakjumbank2/hakjumbank2/service/posts/PostsService.java
@@ -2,11 +2,15 @@ package com.moon.jakjumbank2.hakjumbank2.service.posts;
 
 import com.moon.jakjumbank2.hakjumbank2.domain.posts.Posts;
 import com.moon.jakjumbank2.hakjumbank2.domain.posts.PostsRepository;
+import com.moon.jakjumbank2.hakjumbank2.web.dto.PostsListResponseDto;
 import com.moon.jakjumbank2.hakjumbank2.web.dto.PostsSaveRequestDto;
 import com.moon.jakjumbank2.hakjumbank2.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
