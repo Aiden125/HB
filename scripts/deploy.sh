7bfa1574f4bb29136d5a200a8a224aafbe86b15a
DESKTOP-KVGJO43\mungm <mungml1995@gmail.com>
2024-06-28 22:04:40 +0900

    appspect path, deploy.sh log path change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,34 @@
-#!/bin/bash
+# !/bin/bash
 # jar 위치 변수 지정
-BUILD_JAR=$(ls /home/ec2-user/jenkins/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/app/step1/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
+echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/app/step1/deploy.log
 
-echo ">>> build 파일 복사" >> /home/ec2-user/deploy.log
+echo ">>> build 파일 복사" >> /home/ec2-user/app/step1/deploy.log
 DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
+echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/app/step1/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/app/step1/deploy.log
 else
   echo ">>> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+echo "> 새 애플리케이션 배포" >> /home/ec2-user/app/step1/deploy.log
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME" >> /home/ec2-user/app/step1/deploy.log
+
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties,\
+/home/ec2-user/app/application-oauth.properties,\
+/home/ec2-user/app/application-real-db.properties,\
+classpath:/application-real.properties \
+  -Dspring.profiles.active=real \
+  $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
